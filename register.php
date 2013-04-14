44c4458163bb187deb0fca088fb4d653cd58b52d
Robert <robertpaulic@gmail.com>
2013-04-14 19:41:59 +0200

    Including new page: register.php Inputing forms on index.php

---
+++ register.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Register please</title>
+    </head>
+    <body>
+        <p>Fill the form below to register</p>
+        <form name="input" action="" method="">
+            Choose username:<input type="text" name="new_user"><br />
+            Choose password:<input type="password" name="new_pass"><br />
+            Repeat password:<input type="password" name="rep_pass"><br />
+            Enter email:<input type="email" name="email"><br />
+            <input type="submit" value="Submit">
+        </form>
+        <a href="/exerciseDMS/index.php">Already a member?</a>
+    </body>
+</html>
