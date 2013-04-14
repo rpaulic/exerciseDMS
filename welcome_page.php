a87365bf5ea275655d149296438d76763ae1efab
Robert <robertpaulic@gmail.com>
2013-04-14 19:04:06 +0200

    Including new page: welcome_page.php

---
+++ welcome_page.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Welcome to my Data Management System</title>
+    </head>
+    <body>
+        
+        
+        
+    </body>
+</html>
