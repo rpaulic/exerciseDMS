745aa0f540028c164a2de73c5d22cb91c1ccd997
Robert <robertpaulic@gmail.com>
2013-04-14 20:32:44 +0200

    Dealing with forms on: register.php Dealing with forms on: index.php

--- register.php
+++ register.php
@@ -2,16 +2,28 @@
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title>Register please</title>
+        <title>Register</title>
     </head>
     <body>
         <p>Fill the form below to register</p>
         <form name="input" action="" method="">
-            Choose username:<input type="text" name="new_user"><br />
-            Choose password:<input type="password" name="new_pass"><br />
-            Repeat password:<input type="password" name="rep_pass"><br />
-            Enter email:<input type="email" name="email"><br />
-            <input type="submit" value="Submit">
+            <fieldset>
+                <label>Choose username:
+                    <input type="text" name="new_user"><br />
+                </label>
+                <label>Choose password:
+                    <input type="password" name="new_pass"><br />
+                </label>
+                <label>Repeat password:
+                    <input type="password" name="rep_pass"><br />
+                </label>
+                <label>Enter email:
+                    <input type="email" name="email"><br />
+                </label>
+                <label>
+                    <input type="submit" value="Submit">
+                </label>
+            </fieldset>
         </form>
         <a href="/exerciseDMS/index.php">Already a member?</a>
     </body>
