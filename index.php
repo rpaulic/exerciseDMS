f5fe3c0ee6b6c46357c8d64581acd31dfbfe4fa3
Robert <robertpaulic@gmail.com>
2013-04-16 19:08:50 +0200

    Exploring MVP. Work in progress.

--- index.php
+++ index.php
@@ -1,33 +1,32 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title>Welcome</title>
-    </head>
-    <body>
-        <?php include 'connect_db.php'; ?>
-        <header>
-            <p>Please, log into the Data Management System:</p>
-        </header>
-        
-        <form name="input" action="" method="">
-            <fieldset>
-                <lebel>
-                    Username: <input type="text" name="usr"><br />
-                </lebel>
-                <lebel>
-                    Password: <input type="password" name="pwd"><br />
-                </lebel>
-                <label>
-                    <input type="submit" value="Log in">
-                </label>
-            </fieldset>
-        </form>
-        
-        <a href="/exerciseDMS/register.php">Not a member?</a>
-        
-        <footer>
-            <p>Created by: Robert Paulić</p>
-        </footer>
-    </body>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <title>Welcome</title>
+</head>
+<body>
+    <?php include 'connect_db.php'; ?>
+    
+    <header>
+        <p>Please, log into the Data Management System:</p>
+    </header>
+
+    <form name="input" action="" method="">
+        <fieldset>
+            <lebel>
+                Username: <input type="text" name="user"><br />
+            </lebel>
+            <lebel>
+                Password: <input type="password" name="pwd"><br />
+            </lebel>
+            <label>
+                <input type="submit" value="Log in">
+            </label>
+        </fieldset>
+    </form>
+
+    <a href="/exerciseDMS/register.php">Not a member?</a>
+
+    <footer></footer>
+</body>
 </html>
---
+++ views/template.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8" />
+        <link rel="stylesheet" type="text/css" href="/stylesheets/default.css" />
+        <title></title>
+    </head>
+    <body>
+        
+    </body>
+</html>
